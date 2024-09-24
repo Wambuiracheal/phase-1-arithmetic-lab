caef926d77b4d13fa8f7ce548e175b3e205ac0c5
Racheal <rachealnjugush@gmail.com>
2024-09-25 01:18:22 +0300

    operators

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+//multiply to give me 32
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+//greater integer than 10
+let random = Math.floor(Math.random() * 100) + 1;
+
+//mod that calculates remainder equal to 4
+let num3 = 24;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+//return 20 as max
+let max = Math.max(1, 5, 10, 15, 20);
